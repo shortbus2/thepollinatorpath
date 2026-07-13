a5937e8cd4a9032801df4f8fb2416cf253fd09de
shortbus2 <m45mz99wzd@privaterelay.appleid.com>
2026-07-13 17:35:43 -0600

    Garden Brain: #62 · Pineleaf Penstemon · First bloom

--- observations.js
+++ observations.js
@@ -1,6 +1,6 @@
 window.OBSERVATIONS = [
   {
-    "id": "2026-07-13-mrjuz62o",
+    "id": "2026-07-13-mrjv1r5e",
     "date": "2026-07-13",
     "title": "#62 · Pineleaf Penstemon · First bloom",
     "type": "observation",
@@ -21,10 +21,10 @@ window.OBSERVATIONS = [
     "confidence": "confirmed",
     "public": true,
     "featured": false,
-    "notes": "The little beautiful plant that could",
+    "notes": "",
     "setHero": false,
     "photos": [
-      "images/observations/2026/2026-07-13-mrjuz62o/2026-07-13-01-jpg.jpg"
+      "images/observations/2026/2026-07-13-mrjv1r5e/2026-07-13-01-jpg.jpg"
     ]
   }
 ];
