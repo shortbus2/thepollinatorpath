b22515b23d3cf4d94ab3ca414e422fc9dde2980b
shortbus2 <m45mz99wzd@privaterelay.appleid.com>
2026-07-15 18:00:15 -0600

    Garden Brain: 🐸 Big Booty Judy · Resting

--- observations.js
+++ observations.js
@@ -1,7 +1,7 @@
 window.OBSERVATIONS = [
   {
-    "id": "2026-06-05-mrmqc198",
-    "date": "2026-06-05",
+    "id": "2025-07-24-mrmqszjt",
+    "date": "2025-07-24",
     "title": "🐸 Big Booty Judy · Resting",
     "type": "walk",
     "primary": {
@@ -21,16 +21,15 @@ window.OBSERVATIONS = [
     "confidence": "high",
     "public": true,
     "featured": false,
-    "notes": "Meet Big Booty Judy and all her glory! She’s been a regular in our garden for 3 years running. We’re happy to have her live here, rent free. Although if she wants to do some dishes, we wouldn’t be sad.",
-    "setHero": true,
+    "notes": "",
+    "setHero": false,
     "privacyReview": {
       "completed": true,
       "safeHomepage": true,
-      "reviewedAt": "2026-07-15T23:47:00.572Z"
+      "reviewedAt": "2026-07-16T00:00:11.513Z"
     },
     "photos": [
-      "images/observations/2026/2026-06-05-mrmqc198/photo-1-jpg.jpg",
-      "images/observations/2026/2026-06-05-mrmqc198/photo-2-jpg.jpg"
+      "images/observations/2025/2025-07-24-mrmqszjt/photo-1-jpg.jpg"
     ]
   }
 ];
