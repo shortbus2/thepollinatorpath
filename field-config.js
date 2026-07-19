a91291835283f64f4b306943da9c45d9be319327
shortbus2 <m45mz99wzd@privaterelay.appleid.com>
2026-07-19 15:28:30 -0600

    changed path staging

--- field-config.js
+++ field-config.js
@@ -1,5 +1,5 @@
 window.FIELD_NOTEBOOK_CONFIG = {
-  apiUrl: "https://pollinator-path-garden-brain.y7rv6h85s2.workers.dev",
+  apiUrl: "https://pollinator-path-garden-brain-staging.y7rv6h85s2.workers.dev",
   mode: "cloud",
   version: "4.3.1-beta.4"
 };
